0a484df67d0405ab1fbd661f13afc01248ad8b13
Jared Whalen <jared.m.whalen@gmail.com>
2024-11-06 20:15:20 -0500

    remove build?

--- deploy.sh
+++ deploy.sh
@@ -42,7 +42,7 @@ PROJECT_FOLDER="./build"
 PROJECT_FILE='index.html'
 PUBLIC_URL="$PUBLIC_PATH/$PROJECT_SLUG/$PROJECT_FILE"
 
-npm run build
+# npm run build
 
 sed -i '' 's/\/delaware-online\/storytelling-embeds/https:\/\/www.gannett-cdn.com\/delaware-online\/storytelling-embeds/g' "$PROJECT_FOLDER/index.html"
 sed -i '' 's/<head>/''/' "$PROJECT_FOLDER/index.html"
